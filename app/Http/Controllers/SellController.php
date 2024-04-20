00816c20a1b021294eef899b506403f6036eca53
Aathish R Viswam <aathishrv@gmail.com>
2024-04-20 18:23:27 +0530

    sellPage

---
+++ app/Http/Controllers/SellController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SellController extends Controller
+{
+    //
+
+    public function Sell()
+    {
+        return view('sellFood');
+    }
+}
