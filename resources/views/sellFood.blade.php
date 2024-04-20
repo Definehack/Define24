f1d2abace087a4f7fb8a6481b5ac6e7a6a83073a
rehanphilipjacob <rehanphilipjacob@gmail.com>
2024-04-20 22:37:31 +0530

    Added ui@sellFood

--- resources/views/sellFood.blade.php
+++ resources/views/sellFood.blade.php
@@ -15,18 +15,27 @@
                             <input type="file" name="photo" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
                           </div>
                           <div class="form-group mb-3 w-50">
-                            <label>Select A Type</label>
-                        <select name ="foodCategory" class="form-select" aria-label="Default select example">
+                            <label>Select a type</label>
+                        <select name ="foodCategory" class="form-select mb-3" aria-label="Default select example">
                             <option value="" selected>Open this select menu</option>
                             <option value="fruits">Fruits</option>
                             <option value="vegetables">Vegetables</option>
                             <option value="grains">Grains</option>
                             <!-- Add more options as needed -->
                         </select>
-
-                        <input type="text" name="foodItemName" placeholder="Name of Food Item" />
-                        <input type="text" name="quantity" placeholder="Quantity" />
-                        <input name="Price" placeholder="Price" type="number" />
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"></span>
+                            <input name="foodItemName" placeholder="Name of Food Item" type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"></span>
+                            <input name="quantity" placeholder="Quantity" type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                        </div>
+                        
+                        <div class="input-group mb-3">
+                            <span class="input-group-text">₹</span>
+                            <input name="Price" placeholder="Price" type="number" class="form-control" aria-label="Amount (to the nearest dollar)">
+                        </div>
                         <input type='submit' value="Sell Food" />
                     </form>
                 </div>
